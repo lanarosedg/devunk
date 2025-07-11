048cef7209c5f9ea5279a973167eed7bbcdb4c6f
Lana Rose <lanarosedeguzman@gmail.com>
2025-07-11 19:10:16 +0800

    Fixed navigation

--- src/Home.jsx
+++ src/Home.jsx
@@ -6,62 +6,19 @@ import logo from './assets/logo.png';
 import menu from './assets/menu.png';
 import cart from './assets/cart.png';
 
+import Navigation from './Navigation'
+
 function Home() {
   const navigate = useNavigate();
   return (
     <>
-      <div className="homeContainer">
-        {/* devunk(); cart menu */}
-        <img 
-            src={homePhoto}
-            alt="" 
-            className="homeImage" 
-        />
-        <div className="navTopContainer">
-            <div className="navTopContainerFlex">
-                <div className="navLogoContainer">
-                    <img 
-                        src={logo}
-                        alt="" 
-                        className="navLogo"
-                    />
-                </div>
-                <div className="navMenuContainer">
-                    <img 
-                        src={cart}
-                        alt="" 
-                        className="cart" 
-                    />
-                    <img 
-                    src={menu}
-                    alt="" 
-                    className="menu" 
-                    />
-                </div>
-            </div>
-        </div>
-        <div className="navBottomContainer">
-            <div className="navBottomContainerFlex">
-                <button>
-                    WOMEN
-                </button>
-                <button>
-                    MEN
-                </button>
-                <button>
-                    UNISEX
-                </button>
-            </div>
-        </div>
-
+    <Navigation/>
         <div className="headlineContainer">
           <p className="homeHeadline">
             made by a <span className="developer">developer,</span>
-            <br />
-            for <span className="developer">developers</span>
+            <br />for <span className="developer">developers</span>
           </p>
         </div>
-      </div>
     </>
   )
 }
