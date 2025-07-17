2cf9f80e8562899ca0602ccbe3c22e6de04e7c47
Lana Rose <lanarosedeguzman@gmail.com>
2025-07-17 21:31:30 +0800

    Added peripheral 2

--- src/Peripherals.jsx
+++ src/Peripherals.jsx
@@ -1,6 +1,7 @@
 import Navigation from "./Navigation"
 import mouse1 from './assets/peripherals/mousepadBlack.png'
 import cart from './assets/cart.png'
+import toteWhite from './assets/peripherals/toteWhite.png'
 
 function Peripherals () {
     return (
@@ -15,6 +16,29 @@ function Peripherals () {
                 />
                 <p className="namePrice">
                     <p className="bold">MOUSE PAD</p>
+                    <p>₱200.00</p>
+                </p>
+                <div className="addBuy">
+                    <div className="addBuyFlex">
+                        <img 
+                            src={cart}
+                            alt="" 
+                            className="cart" 
+                        />
+                    <button className="buy">
+                        BUY
+                    </button>
+                </div>
+                    </div>
+            </div>
+            <div className="product1Container">
+                <img 
+                    src={toteWhite}
+                    alt="" 
+                    className="toteWhite" 
+                />
+                <p className="namePrice">
+                    <p className="bold">LAPTOP TOTE BAG</p>
                     <p>₱200.00</p>
                 </p>
                 <div className="addBuy">
